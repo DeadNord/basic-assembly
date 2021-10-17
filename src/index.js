ff1ad3d55de5b2dafb5f1f0711c4f8c526c8ce69
DeadNord <eosipopo@gmail.com>
2021-10-17 19:45:08 +0300

    v7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+// import model from './layouts/test.hbs';
+// import content from './js/json/test.json';
+
 import './js/modual-1.js';
 import './js/import-img.js';
 import './sass/main.scss';
+
+// // Card Insert
+// const cardsMarkup = model(content);
+// refs.list.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// // Links
+// const ref = {
+//   list: document.querySelector('.hero'),
+// };
+
+// // Target
+// localStorage.removeItem('content');
+// ref.list.addEventListener('click', e => {
+//   localStorage.setItem('content', `${e.target.id}`);
+//   catalogRender();
+// });
